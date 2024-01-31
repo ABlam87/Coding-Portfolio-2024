a1c7fa7af39d161e3d227eb742e0ca856279ca46
ABlam87 <adamblampied@gmail.com>
2024-01-31 22:19:17 +0000

    have moved images to online server to fix image loading issue

--- portfolio/src/pages/Contact.jsx
+++ portfolio/src/pages/Contact.jsx
@@ -13,22 +13,22 @@ const Contact = function() {
 
         <div className="col-md-12 contactCopy row">
             <div className="card">
-                <img src="./public/assets/images/email icon.png" style={{width: "50px"}}></img>
+                <img src="https://i.imgur.com/gIf1mef.png" style={{width: "50px"}}></img>
                 <p>adamblampied@gmail.com</p>
             </div>
 
             <div className="card">
-        <       img src="./public/assets/images/Mobile-Smartphone-Icon.png" style={{width: "50px"}}></img>
+        <       img src="https://i.imgur.com/emyKw2L.png" style={{width: "50px"}}></img>
                 <p>(+44)7966039290</p>
             </div>    
         
             <div className="card">
-                <img class='ig' src="./public/assets/images/X_IG_icon.png" style={{width: "110px"}}></img>
+                <img class='ig' src="https://i.imgur.com/lryAZpA.png" style={{width: "110px"}}></img>
                 <p>@adamtheblampied</p>
             </div>
 
             <div className="card">
-                <img src='./public/assets/images/linkedin icon.png'></img>
+                <img src='https://i.imgur.com/baprjgl.png'></img>
                 <p><a href='https://www.linkedin.com/in/adam-blampied-7065a12b1' target="_blank">LinkedIn Profile</a></p>
             </div>    
     </div>
